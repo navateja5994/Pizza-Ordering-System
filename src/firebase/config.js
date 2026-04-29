8cedd37f093c471b3b75051ac13bf8f257769120
N.NAVATEJA REDDY <navatejar80@gmail.com>
2026-04-29 10:12:26 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,14 +3,14 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDQHjV9ERWkMcnguDkWNXYQKveVGoPfnTQ",
+  authDomain: "pizza-ordering-system-61998.firebaseapp.com",
+  databaseURL: "https://pizza-ordering-system-61998-default-rtdb.firebaseio.com",
+  projectId: "pizza-ordering-system-61998",
+  storageBucket: "pizza-ordering-system-61998.firebasestorage.app",
+  messagingSenderId: "993779182987",
+  appId: "1:993779182987:web:54d41d7ed096062f24138e",
+  measurementId: "G-EPMP8X0QBX"
 };
 
 const app = initializeApp(firebaseConfig);
